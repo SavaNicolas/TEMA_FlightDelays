3bf19d5ac64204b24d0417b5a3d3d8ec6f205595
SavaNicolas <s311650@studenti.polito.it>
2025-06-21 16:23:25 +0200

    update query nodi

--- database/DAO.py
+++ database/DAO.py
@@ -15,16 +15,16 @@ class DAO():
         #conto prima il numero di voli che fa una compagnia
         #e da quello poi conto quante compagnie partono da quell'areoporto
         #a noi servono solo quelli con + di 5 compagnie(having)
-        query = """SELECT t.ID, t.IATA_CODE, COUNT(*) AS N
+        query = """SELECT t.ID, COUNT(*) AS N
 FROM (
-    SELECT a.ID, a.IATA_CODE, f.AIRLINE_ID, COUNT(*)
+    SELECT a.ID, f.AIRLINE_ID
     FROM airports a, flights f
     WHERE a.ID = f.ORIGIN_AIRPORT_ID OR a.ID = f.DESTINATION_AIRPORT_ID
-    GROUP BY a.ID, a.IATA_CODE, f.AIRLINE_ID
+    GROUP BY a.ID, f.AIRLINE_ID
 ) t
-GROUP BY t.ID, t.IATA_CODE
-HAVING N >= %s
-ORDER BY N ASC;
+GROUP BY t.ID
+HAVING N >= 5
+ORDER BY N ASC
 
         """
 
